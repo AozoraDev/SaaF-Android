b6b568183375cafb179e5e98fe97ba5c74ecffcf
AozoraDev <niicoremusic@gmail.com>
2021-06-07 21:02:41 +0800

    Initial

---
+++ app/src/main/java/com/aozoradev/saaf/MainActivity.java
@@ -0,0 +1,87 @@
+package com.aozoradev.saaf;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.ListView;
+import android.view.View;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import androidx.core.app.ActivityCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private Button button;
+    private AlertDialog.Builder dialog;
+    private ListView listView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialize(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+        } else {
+            initializeLogic();
+        }
+    }
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000) {
+            initializeLogic();
+        }
+	}
+    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, 0, "Dark Mode").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        return true;
+	}
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0 :
+                Toast.makeText(this, "Still working on it", Toast.LENGTH_SHORT).show();
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    private void initialize(Bundle _savedInstanceState) {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        button = (Button) findViewById(R.id.button);
+        listView = (ListView) findViewById(R.id.listView);
+        setSupportActionBar(toolbar);
+        listView.setVisibility(View.GONE);
+    }
+    
+    private void initializeLogic () {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            dialog = new AlertDialog.Builder(this).setCancelable(false).setMessage("This app requires storage access to work properly. Please grant storage permission.");
+            dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface _dialog, int _which) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+                    }
+                });
+            dialog.create().show();
+        }
+    }
+    
+    private void readZip () {
+        
+    }
+}
