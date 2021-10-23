6b0b95f31354b9b51757a115eebe412df43ba235
AozoraDev <niicoremusic@gmail.com>
2021-10-24 07:12:20 +0800

    Update ReadOsw.java

--- app/src/main/java/com/aozoradev/saaf/ReadOsw.java
+++ app/src/main/java/com/aozoradev/saaf/ReadOsw.java
@@ -9,13 +9,30 @@ import java.util.zip.ZipEntry;
 
 public class ReadOsw {
   public static void load (String fileName, ArrayList<String> listItems) throws IOException {
-        try (FileInputStream fis = new FileInputStream(fileName);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        ZipInputStream zis = new ZipInputStream(bis)) { 
-            ZipEntry ze;
-            while ((ze = zis.getNextEntry()) != null) {
-                listItems.add(ze.getName());
-            }
-        }
+    FileInputStream fis = null;
+    BufferedInputStream bis = null;
+    ZipInputStream zis = null;
+    
+    try {
+      fis = new FileInputStream(fileName);
+      bis = new BufferedInputStream(fis);
+      zis = new ZipInputStream(bis); 
+      ZipEntry ze;
+      int index = 0;
+      while ((ze = zis.getNextEntry()) != null) {
+          index = ++index;
+          listItems.add(ze.getName() + index);
+      }
+    } finally {
+      if (zis != null) {
+        zis.close();
+      }
+      if (bis != null) {
+        bis.close();
+      }
+      if (fis != null) {
+        fis.close();
+      }
     }
+  }
 }
