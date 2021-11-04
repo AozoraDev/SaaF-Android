215141a92e7bf18fafcf5957d9ebaecf1d66b4f0
AozoraDev <niicoremusic@gmail.com>
2021-11-04 18:39:45 +0800

    More fixes

--- app/src/main/java/com/aozoradev/saaf/MainActivity.java
+++ app/src/main/java/com/aozoradev/saaf/MainActivity.java
@@ -5,6 +5,7 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.core.app.ActivityCompat;
 import androidx.documentfile.provider.DocumentFile;
@@ -15,10 +16,15 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.net.URISyntaxException;
 
+import android.os.Handler;
 import android.os.Bundle;
 import android.os.Build;
+import android.os.Looper;
+import android.graphics.drawable.ColorDrawable;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.content.Intent;
@@ -33,11 +39,13 @@ import android.view.MenuItem;
 import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
-  private Toolbar toolbar;
   private Button button;
   private RecyclerView recyclerView;
   private ArrayList<Radio> radio;
-  private MaterialAlertDialogBuilder backPressedDialog;
+  private AlertDialog backPressedDialog;
+  private AlertDialog loading;
+  private ExecutorService executor;
+  private Handler handler;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onDestroy() {
     super.onDestroy();
+    executor.shutdown();
+    executor.shutdownNow();
     System.exit(0);
   }
 
@@ -70,34 +80,40 @@ public class MainActivity extends AppCompatActivity {
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
     if (requestCode == 200 && resultCode == Activity.RESULT_OK) {
-      Uri uri = data.getData();
-
-      DocumentFile df = DocumentFile.fromSingleUri(getApplicationContext(), uri);
-      String nodeName = df.getName();
-
-      try {
-        radio = Radio.createRadioList(this, uri, nodeName.replaceAll(".osw", ""));
-        boolean isEqual = Arrays.asList(Constant.stationName).contains(nodeName);
-        
-        if (isEqual == false) {
-          Util.toast(this, "Failed to load the file");
+      executor.execute(() -> {
+        handler.post(() -> loading.show());
+        Uri uri = data.getData();
+        DocumentFile df = DocumentFile.fromSingleUri(getApplicationContext(), uri);
+        String nodeName = df.getName();
+        try {
+          radio = Radio.createRadioList(MainActivity.this, uri, nodeName.replaceAll(".osw", ""));
+        } catch (IOException err) {
+          handler.post(() -> {
+            Util.toast(MainActivity.this, err.getMessage());
+            loading.dismiss();
+          });
+          err.printStackTrace();
           return;
-        } else if (radio.isEmpty()) {
-          Util.toast(this, "Failed to load the file");
+        }
+        boolean isEqual = Arrays.asList(Constant.stationName).contains(nodeName);
+        if ((isEqual == false) || (radio.isEmpty())) {
+          handler.post(() -> {
+            Util.toast(MainActivity.this, "Failed to load the file");
+            loading.dismiss();
+          });
           return;
         }
         
-        RadioAdapter adapter = new RadioAdapter(radio);
-        
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setVisibility(View.VISIBLE);
-        button.setVisibility(View.GONE);
-        getSupportActionBar().setSubtitle(Constant.station);
-      } catch (IOException err) {
-        Util.toast(this, err.getMessage());
-        err.printStackTrace();
-      }
+        handler.post(() -> {
+          RadioAdapter adapter = new RadioAdapter(radio);
+          recyclerView.setAdapter(adapter);
+          recyclerView.setLayoutManager(new LinearLayoutManager(this));
+          recyclerView.setVisibility(View.VISIBLE);
+          button.setVisibility(View.GONE);
+          getSupportActionBar().setSubtitle(Constant.station);
+          loading.dismiss();
+        });
+      });
     }
   }
 
@@ -110,18 +126,31 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void initialize(Bundle _savedInstanceState) {
-    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     button = (Button) findViewById(R.id.button);
     recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
     setSupportActionBar(toolbar);
+    
     backPressedDialog = new MaterialAlertDialogBuilder(MainActivity.this)
-    .setCancelable(true).setMessage("Are you sure you want to close this app?")
-    .setPositiveButton("YES", (_dialog, _which) -> finish())
-    .setNegativeButton("NO", null);
+    .setCancelable(true)
+    .setMessage("Are you sure you want to close this app?")
+    .setPositiveButton("YES", (_which, _dialog) -> finish())
+    .setNegativeButton("NO", null)
+    .create();
+    
+    loading = new AlertDialog.Builder(this)
+    .setCancelable(false)
+    .setView(View.inflate(this, R.layout.haha_custom_progress_bar_layout_go_bbrrrr, null))
+    .create();
+    loading.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+    
     RecyclerView.ItemDecoration divider = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
     recyclerView.setHasFixedSize(true);
     recyclerView.addItemDecoration(divider);
     recyclerView.setVisibility(View.GONE);
+    
+    executor = Executors.newSingleThreadExecutor();
+    handler = new Handler(Looper.getMainLooper());
   }
 
   private void initializeLogic() {
@@ -130,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
       .setCancelable(false).setMessage("This app requires storage access to work properly. Please grant storage permission.")
       .setPositiveButton("OK", (_dialog, _which) -> ActivityCompat.requestPermissions(MainActivity.this, Constant.permissions, 1000)).show();
     }
-
+    button.setVisibility(View.VISIBLE);
     button.setOnClickListener(v -> {
       Intent chooseFile = new Intent(Intent.ACTION_OPEN_DOCUMENT);
       chooseFile.setType("*/*");
